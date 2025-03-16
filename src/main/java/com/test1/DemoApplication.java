58688cadb869bafacb58fec8d81826a69f1e1807
hussainfaiz76 <hussainfaiz76@gmail.com>
2025-03-16 13:27:21 +0530

    Update DemoApplication.java

--- src/main/java/com/test1/DemoApplication.java
+++ src/main/java/com/test1/DemoApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		String name = "mike";
+		int x = 100;
+		boolean isPresent = false;
+		int y = 200;
+		char z = 'a';
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
